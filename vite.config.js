66fa5c5ce25b5062fc88649ad40fb87df02c9af3
Mauricio <mauballe11@gmail.com>
2025-03-31 21:55:59 -0600

    Fix lucide-react issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fa', 'lucide-react'],
+      external: ['react-icons/fa'],
     },
   },
 })
